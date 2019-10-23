e0e96b4a56d8c3cef7cc782369a43f8e87d14539
SrKomodo <komodosr@gmail.com>
2019-10-23 08:00:05 -0300

    fix loading settings

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import App from "./app.svelte";
 
 const app = new App({
   target: document.body,
-  props: JSON.parse(localStorage.getItem("props") || {})
+  props: JSON.parse(localStorage.getItem("props") || "{}")
 });
